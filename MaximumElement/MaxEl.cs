c70b16ea7f853aca4c7850abfd7b5df872aeb173
Borislav Tserovski <Borislav.Tserovski@gmail.com>
2017-05-25 20:22:50 +0300

    lastChanges

--- MaximumElement/MaxEl.cs
+++ MaximumElement/MaxEl.cs
@@ -9,9 +9,9 @@ namespace MaximumElement
         static void Main()
         {
             int numberOfElements = int.Parse(Console.ReadLine());
-
+            int maxEl = int.MinValue;
             Stack <int> numbersStack = new Stack<int>();
-
+            Stack<int> maxStack = new Stack<int>();
             for (int i = 0; i < numberOfElements; i++)
             {
                 int[] inputLine = Console.ReadLine().Split()
@@ -20,11 +20,40 @@ namespace MaximumElement
 
                 switch (inputLine[0])
                 {
-                    case 1:  numbersStack.Push(inputLine[1]); 
+                    case 1:
+                    {
+                        if (maxEl < inputLine[1])
+                        {
+                            maxEl = inputLine[1];
+                            maxStack.Push(maxEl);
+                        }
+
+                        numbersStack.Push(inputLine[1]);
+                    }
                         break;
-                    case 2: numbersStack.Pop();
+                    case 2:
+                    {
+                        if (numbersStack.Peek() == maxStack.Peek() && maxStack.Count > 0)
+                        {
+                            maxStack.Pop();
+                            if (maxStack.Count > 0)
+                            {
+                                maxEl = maxStack.Peek();
+                            }
+                            else
+                            {
+                                maxEl = int.MinValue;
+                            }
+                        }
+
+                        numbersStack.Pop();
+
+                    }
                         break;
-                    case 3: Console.WriteLine(numbersStack.ToArray().Max());
+                    case 3:
+                    {
+                        Console.WriteLine(maxStack.Peek());
+                    }
                         break;
                 }
                 
---
+++ SimpleTextEditor/TextEditor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Csharp.Advanced.StacksAndQueues
+{
+    public partial class StacksAndQueues
+    {
+        public static class _10SimpleTextEditor
+        {
+            public static void Main()
+            {
+                int numberOfOperations = int.Parse(Console.ReadLine());
+
+                StringBuilder sb = new StringBuilder();
+                var sbHistory = new Stack<string>();
+
+                while (numberOfOperations-- > 0)
+                {
+                    var inputParams = Console.ReadLine().Split();
+                    var cmd = inputParams[0];
+                    var value = inputParams.Length > 1 ? inputParams[1] : null;
+
+                    switch (cmd)
+                    {
+
+                        case "1":
+                            sbHistory.Push(sb.ToString());
+                            sb.Append(value);
+                            break;
+
+                  
+                        case "2":
+                            sbHistory.Push(sb.ToString());
+                            sb.Length -= int.Parse(value);
+                            break;
+
+                            
+                        case "3":
+                            Console.WriteLine(sb[int.Parse(value) - 1]);
+                            break;
+
+
+
+                        case "4":
+                            sb = new StringBuilder(sbHistory.Pop());
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
